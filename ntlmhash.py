da59af02d33f14f982e4ee27ef20937177f9e7f3
Peter Preeper <ppreeper@gmail.com>
2019-06-14 08:52:35 -0600

    add args to ntlmhash

--- ntlmhash.py
+++ ntlmhash.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import hashlib,binascii
-hash = hashlib.new('md4', "thisismyhashvalue".encode('utf-16le')).digest()
-print(binascii.hexlify(hash))
+import argparse
+import hashlib
+import binascii
+
+def main():
+    parser = argparse.ArgumentParser("ntml hash passwords")
+    parser.add_argument("-p", action="store", dest="p", help="password to hash")
+    args = parser.parse_args()
+
+    if args.p is None:
+        parser.error("no password specified")
+        return
+    p = args.p
+    hash = hashlib.new('md4', p.encode('utf-16le')).digest()
+    print(binascii.hexlify(hash))
+    return
+
+if __name__ == "__main__":
+    main()
